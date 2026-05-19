1abb4b5f5287667365233c08cde72e6d28c06f26
muslumhanerol <mslmhanerol@gmail.com>
2026-05-19 23:47:25 +0300

    Update BackToTopButton.jsx

--- src/components/common/BackToTopButton.jsx
+++ src/components/common/BackToTopButton.jsx
@@ -1,1 +1,6 @@
 import { FaArrowRight } from "react-icons/fa";
+export default function BackToTopButton({show}){
+    return (
+        <button onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}></button>
+    )
+}
