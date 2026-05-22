ab5c4ca3cdfd6367f0bcd0495b593894e78e16f7
muslumhanerol <mslmhanerol@gmail.com>
2026-05-22 23:44:08 +0300

    Create SiteHeader.jsx

---
+++ src/components/layout/SiteHeader.jsx
@@ -0,0 +1,12 @@
+import {FaBars, FaBrain, FaEnvelope} from "react-icons/fa"
+
+export default function SiteHeader({isScrolled, mebuopen, onToggleMenu, onCloseMenu}) {
+    return (
+        <>
+         <header className=>
+
+
+         </header>        
+        </>
+    )
+}
