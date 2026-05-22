a365770d3a4575869ff77f30ac2b60475b4934b0
muslumhanerol <mslmhanerol@gmail.com>
2026-05-22 23:49:50 +0300

    Update SiteHeader.jsx

--- src/components/layout/SiteHeader.jsx
+++ src/components/layout/SiteHeader.jsx
@@ -3,7 +3,7 @@ import {FaBars, FaBrain, FaEnvelope} from "react-icons/fa"
 export default function SiteHeader({isScrolled, mebuopen, onToggleMenu, onCloseMenu}) {
     return (
         <>
-         <header className=>
+         <header className={`fixed left-1/2 top=0 z-50 mt-3 flex w-[min(95%, 72rem)] -translate-x-1/2 items-center justify-between rounded-2xl border px-4 py-3 backdrop-blur-xl transition-all duration-300`}>
 
 
          </header>        
