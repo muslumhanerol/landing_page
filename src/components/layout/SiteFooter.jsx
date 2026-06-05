eeb21648d30eba7db54b4a1ca798c98f2abff67f
muslumhanerol <mslmhanerol@gmail.com>
2026-06-05 23:38:12 +0300

    Update SiteFooter.jsx

--- src/components/layout/SiteFooter.jsx
+++ src/components/layout/SiteFooter.jsx
@@ -4,7 +4,11 @@ export default function SiteFooter() {
     return (
         <footer className="mt-20 border-t border-zinc-800 py-8 text-sm text-zinc-400">
             <div className="flex flex-col items-center gap-4 sm:flex-row sm:justify-between">
-                <div className="flex flex-col items-center gap-3 sm:items-start"></div>
+                <div className="flex flex-col items-center gap-3 sm:items-start">
+                    <div className="flex items-center justify-center gap-5 ">
+                        <a href="mailto:hello@example.com" className="inline-flex items-center gap-2 hover:text-blue-300"></a>
+                    </div>
+                </div>
             </div>
         </footer>
     )
