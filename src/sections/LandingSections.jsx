2f71cbbd36ad8ad9a8147388e2dd79c78cc119e3
muslumhanerol <mslmhanerol@gmail.com>
2026-06-14 23:32:55 +0300

    LandingSection

---
+++ src/sections/LandingSections.jsx
@@ -0,0 +1,26 @@
+import {FaArrowLeft, FaArrowRight, FaCheckCircle} from 'react-icons/fa'
+
+export default function LandingSections({
+    stats,
+    workflowSteps,
+    services,
+    showcases,
+    activeShowcase,
+    setActiveShowcase,
+    features,
+    testimonials,
+    activeTestimonial,
+    setActiveTestimonial,
+    integrations,
+    pricing,
+    annualBilling,
+    setAnnualBilling,
+    team,
+    faqs,
+    openFaq,
+    setOpenFaq
+}) {
+    return (
+        
+    )
+}
