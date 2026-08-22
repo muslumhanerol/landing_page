cbed611a17ab8a5d035468cf18ce025a663abaee
muslumhanerol <mslmhanerol@gmail.com>
2026-08-22 23:31:01 +0300

    Update landingData.js

---
+++ src/data/landingData.js
@@ -0,0 +1,27 @@
+import {
+    FaAws,
+    FaBolt,
+    FaBrain,
+    FaChartLine,
+    FaCloud,
+    FaHubspot,
+    FaLock,
+    FaMicrosoft,
+    FaRegFileAlt,
+    FaRobot,
+    FaSalesforce,
+    FaSlack,
+    FaSnowflake,
+    FaStripe,
+}   from 'react-icons/fa'
+
+export const stats = [
+    {label: 'Models Deployed', value: '280+'},
+    {label: 'Avg. Automation Gain', value: '127%'},
+    {label: 'Enterprise Uptime', value: '99.99%'},
+    {label: 'Global Clients', value: '42'},
+]
+
+export const features = [
+    
+]
