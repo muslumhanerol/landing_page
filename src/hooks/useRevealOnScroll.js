b52027b30ad161c182908fd48b9718f005921a45
muslumhanerol <mslmhanerol@gmail.com>
2026-09-12 23:40:51 +0300

    Update useRevealOnScroll.js

--- src/hooks/useRevealOnScroll.js
+++ src/hooks/useRevealOnScroll.js
@@ -3,5 +3,6 @@ import { useEffect } from "react";
 export function useRevealOnScroll() {
     useEffect(() => {
         const elements = document.querySelectorAll('[data-reveal]')
+        const observer = 
     })
 }
